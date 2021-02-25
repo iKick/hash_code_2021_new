89287c61c0fbc79a0d7dd33f81726bd1d37d0270
kostyshyn <serhii.kostyshyn@ria.com>
2021-02-25 22:57:04 +0200

    тупа підстановка

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const _ = require('lodash')
+const _ = require('lodash');
 const inputMainPath = './inputFile';
 const outputMainPath = './outputFile';
 const files = fs.readdirSync(inputMainPath);
